8a6f682dd1827abcd2aaecb41a706ad071558969
Quadrate <117572566+quadratech188@users.noreply.github.com>
2023-11-04 19:42:11 +0900

    Gui is now working

--- Scripts/PlacementSettingsGUI.lua
+++ Scripts/PlacementSettingsGUI.lua
@@ -10,42 +10,99 @@ function PlacementSettingsGUI:initialize()
 
 	self.gui:createHorizontalSlider("PlacementRadius", 20, 7.5, "onGUIUpdate", true)
 
-	self.roundingModeButtons = {
-		["Center"] = "RoundingMode_Center",
-		["Fixed"] = "RoundingMode_Fixed",
-		["Dynamic"] = "RoundingMode_Dynamic"
+	self.buttons = {
+		roundingMode = {
+			["Center"] = "RoundingMode_Center",
+			["Fixed"] = "RoundingMode_Fixed",
+			["Dynamic"] = "RoundingMode_Dynamic"
+		},
+		clickMode = {
+			[true] = "ClickMode_Twice",
+			[false] = "ClickMode_Once"
+		}
 	}
 
-	for _, button in pairs(self.roundingModeButtons) do
-		self.gui:setButtonCallback(button, "onGUIUpdate")
+	for _, button in pairs(self.buttons.roundingMode) do
+		self.gui:setButtonCallback(button .. "T", "onGUIUpdate")
+		self.gui:setButtonCallback(button .. "F", "onGUIUpdate")
+	end
+
+	for _, button in pairs(self.buttons.clickMode) do
+		self.gui:setButtonCallback(button .. "T", "onGUIUpdate")
+		self.gui:setButtonCallback(button .. "F", "onGUIUpdate")
 	end
 
 	BetterPlacementClass:linkCallback("onGUIUpdate", self.onGUIUpdate, -1)
+
+	self:onGUIUpdate(nil)
 end
 
 
 function PlacementSettingsGUI:onGUIUpdate(data)
+
+	print(data)
 	
 	self = PlacementSettingsGUI
-	
-	if data:sub(1, 12) == "RoundingMode" then -- If it's RoundingMode
+
+	if type(data) == "string" then
 		
-		for index, button in pairs(self.roundingModeButtons) do
-			
-			if button == data then
-				self.gui:setButtonState(button, true)
-				BetterPlacementCoreV2.settings.roundingSetting = index
-			else
-				self.gui:setButtonState(button, false)
+		if data:sub(1, 12) == "RoundingMode" then
+
+			for index, button in pairs(self.buttons.roundingMode) do
+				if string.match(data, button) then
+					BetterPlacementCoreV2.settings.roundingSetting = index
+				end
+			end
+		else -- If it's ClickMode
+
+			for index, button in pairs(self.buttons.clickMode) do
+				
+				if string.match(data, button) then
+					BetterPlacementCoreV2.settings.doubleClick = index
+				end
 			end
 		end
 	end
 
-	
+	if type(data) == "number" then -- If it's PlacementRadius
+		
+		BetterPlacementCoreV2.settings.placementRadius = data
+	end
+
+
+	for index, button in pairs(self.buttons.clickMode) do
+		
+		if index == BetterPlacementCoreV2.settings.doubleClick then
+			
+			self.gui:setVisible(button .. "T", true)
+			self.gui:setVisible(button .. "F", false)
+		
+		else
+			self.gui:setVisible(button .. "T", false)
+			self.gui:setVisible(button .. "F", true)
+		end
+	end
+
+	for index, button in pairs(self.buttons.roundingMode) do
+		
+		if index == BetterPlacementCoreV2.settings.roundingSetting then
+			
+			self.gui:setVisible(button .. "T", true)
+			self.gui:setVisible(button .. "F", false)
+		
+		else
+			self.gui:setVisible(button .. "T", false)
+			self.gui:setVisible(button .. "F", true)
+		end
+	end
 end
 
 
 function PlacementSettingsGUI:onToggle()
+
+	self.gui:setButtonState(self.buttons.roundingMode[BetterPlacementCoreV2.settings.roundingSetting], true)
+
+	self.gui:setButtonState(self.buttons.clickMode[BetterPlacementCoreV2.settings.doubleClick], true)
 	
 	self.gui:open()
 end
